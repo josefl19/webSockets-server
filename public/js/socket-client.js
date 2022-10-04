64103887bc13a3022f4a7abce3fae8692395f399
josefl <jose.fl_@hotmail.com>
2022-10-04 17:27:28 -0500

    Message of connection and disconnect in Front-End side

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,2 +1,18 @@
+// Referencia al HTML
+const lblOnline = document.querySelector('#lblOnline');
+const lblDisconnect = document.querySelector('#lblDisconnect');
+
 // Socket del cliente
 const socket = io();
+
+socket.on('connect', () => {
+    console.log('Conectado al servidor');
+    lblDisconnect.style.display = 'none';
+    lblOnline.style.display = '';
+})
+
+socket.on('disconnect', () => {
+    console.log('Desconectado del servidor');
+    lblOnline.style.display = 'none';
+    lblDisconnect.style.display = '';
+})
