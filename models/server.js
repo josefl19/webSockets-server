f846c456cb8879a3faa473bb59209b9006b9adfe
josefl <jose.fl_@hotmail.com>
2022-10-04 18:07:54 -0500

    Emit an event from client to server

--- models/server.js
+++ models/server.js
@@ -38,16 +38,16 @@ class Servidor {
         this.io.on("connection", (socket) => {
             // send a message to the client
             //socket.emit("hello from server", 1, "2", { 3: Buffer.from([4]) });
-          
-            // receive a message from the client
-            //socket.on("hello from client", (...args) => {
-
-            // ...
                 console.log('Cliente conectado', socket.id );
 
                 socket.on('disconnect', () => {
                     console.log('Cliente desconectado!', socket.id);
-                })
+                });
+
+                // receive a message from the client
+                socket.on('enviar-mensaje', ( payload ) => {
+                    console.log( payload );
+                });
             });
     }
 
--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,6 +1,8 @@
 // Referencia al HTML
 const lblOnline = document.querySelector('#lblOnline');
 const lblDisconnect = document.querySelector('#lblDisconnect');
+const txtMensaje = document.querySelector('#txtMensaje');
+const btnEnviar = document.querySelector('#btnEnviar');
 
 // Socket del cliente
 const socket = io();
@@ -15,4 +17,17 @@ socket.on('disconnect', () => {
     console.log('Desconectado del servidor');
     lblOnline.style.display = 'none';
     lblDisconnect.style.display = '';
+})
+
+// Send information from client to server
+btnEnviar.addEventListener('click', () => {
+    const mensaje = txtMensaje.value;
+
+    // Send a object
+    const payload = {
+        mensaje,
+        fecha: new Date().getTime(),
+        id: '123ABC'
+    }
+    socket.emit('enviar-mensaje', payload)
 })
